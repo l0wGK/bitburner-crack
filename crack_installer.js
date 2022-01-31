dece5017b82dd09596f28f772b11381b60cfd886
l0wGK <87951785+l0wGK@users.noreply.github.com>
2022-01-31 12:19:51 +0100

    Update crack_installer.js

--- crack_installer.js
+++ crack_installer.js
@@ -2,13 +2,13 @@
 export async function main(ns) {
 	ns.disableLog('ALL');
 
-	await ns.wget('https://github.com/l0wGK/bitrunner-crack/blob/main/crack_2.js', 'crack_2.js');
+	await ns.wget('https://raw.githubusercontent.com/l0wGK/bitburner-crack/main/crack_2.js', 'crack_2.js');
 	ns.print("Installed crack_2.js");
-	await ns.wget('https://github.com/l0wGK/bitrunner-crack/blob/main/track_2.js', 'track_2.js');
+	await ns.wget('https://raw.githubusercontent.com/l0wGK/bitburner-crack/main/track_2.js', 'track_2.js');
 	ns.print("Installed track_2.js");
-	await ns.wget('https://github.com/l0wGK/bitrunner-crack/blob/main/hack_2.js', 'hack_2.js');
+	await ns.wget('https://raw.githubusercontent.com/l0wGK/bitburner-crack/main/hack_2.js', 'hack_2.js');
 	ns.print("Installed hack_2.js")
-	await ns.wget('https://github.com/l0wGK/bitrunner-crack/blob/main/transactions.js', 'transactions.js');
+	await ns.wget('https://raw.githubusercontent.com/l0wGK/bitburner-crack/main/transactions.js', 'transactions.js');
 	ns.print("Installed transactions.js");
 	ns.print("You may now kill this script");
 
